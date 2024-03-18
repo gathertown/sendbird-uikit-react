73d73f3c8f676b79cba3aee89fb319270cf13732
Andy Lui <alui@gather.town>
2024-03-18 10:22:52 -0700

    [APP-6458] - support pass through edit input (#13)

--- src/modules/Channel/components/ChannelUI/index.tsx
+++ src/modules/Channel/components/ChannelUI/index.tsx
@@ -16,6 +16,8 @@ export interface ChannelUIProps extends GroupChannelUIBasicProps {
    * Customizes all child components of the message component.
    * */
   renderMessage?: GroupChannelUIBasicProps['renderMessage'];
+
+  renderEditInput?: GroupChannelUIBasicProps['renderEditInput'];
 }
 
 const ChannelUI = (props: ChannelUIProps) => {
